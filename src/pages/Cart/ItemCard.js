b4eebea77de1d979f79eaa53a3263d824226dbe4
sravani <suravarapusravani3@gmail.com>
2023-12-06 21:18:26 +0530

    commit all changes

--- src/pages/Cart/ItemCard.js
+++ src/pages/Cart/ItemCard.js
@@ -12,10 +12,6 @@ const ItemCard = ({ item }) => {
   return (
     <div className="w-full grid grid-cols-5 mb-4 border py-2">
       <div className="flex col-span-5 mdl:col-span-2 items-center gap-4 ml-4">
-        <ImCross
-          onClick={() => dispatch(deleteItem(item._id))}
-          className="text-primeColor hover:text-red-500 duration-300 cursor-pointer"
-        />
         <img className="w-32 h-32" src={item.image} alt="productImage" />
         <h1 className="font-titleFont font-semibold">{item.name}</h1>
       </div>
